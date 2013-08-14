6856c3282e78412cc8c5e8906f627bb473490482
Shumilkin Gena <shumilkin.gena@yandex.ru>
2013-08-14 20:03:02 +0400

    fix call comments

--- app/controllers/topics/comments_controller.rb
+++ app/controllers/topics/comments_controller.rb
@@ -6,12 +6,12 @@ class Stories::CommentsController < ApplicationController
   # код выглядит по меньшей мере странно, но как решить проблему с обнулением переменной @story 
   # пока не знаю, поэтому оставлю как есть до лучших времен.
   def create
-    @comment = StoryComment.new(params[:topic_comment])
-    @comment.story = @story
+    comment = StoryComment.new(params[:topic_comment])
+    comment.story = @story
     
-    @comment.save 
+    comment.save 
     
-    @comments = @story.comments.sort{|x,y| x.created_at <=> y.created_at}
+    @comments = @story.comments.all
     @comment = @story.comments.build
 
     render json: @comments, status: 201
@@ -20,7 +20,7 @@ class Stories::CommentsController < ApplicationController
   def destroy
     @comment = StoryComment.find(params[:id])
     @comment.destroy
-    @comments = @story.comments.sort{|x,y| x.created_at <=> y.created_at}
+    @comments = @story.comments.all
 
     render json: @comments, status: 201
   end
