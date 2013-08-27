80d2920b3e16dde2a068de09e1a6b85e53d5118c
Shumilkin Gena <shumilkin.gena@yandex.ru>
2013-08-27 14:00:37 +0400

    fix mailer: subject to locales

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < BaseMailer
 
   def send_welcome_email(user)
     @user = user
-    mail(to: user.email, subject: 'Welcome to simple task manager')
+    mail(to: user.email)
   end
 
 end
