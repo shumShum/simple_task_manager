c2ac024e266be07de3b90f78d1adfeb9b034d0e8
Shumilkin Gena <shumilkin.gena@yandex.ru>
2013-08-27 13:51:07 +0400

    fix problem

--- app/controllers/api/v1/stories/comments_controller.rb
+++ app/controllers/api/v1/stories/comments_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::Stories::CommentsController < Api::V1::Stories::ApplicationContro
     # TODO это нужно обсудить
     # такой вариант красивей и теперь (после 'has_many comments, class_name: CommentsNewType')
     # но тогда так нужно делать и в стори (для единообразия), но там все не так просто (у стори несколько типов)
-    # плюс ко всему все эти пертурбации привели к тому, что 'user.stories.comments' выдает ошибку (логично)
     @comment = resource_story.comments.build(params[:story_comment])
     @comment.user = current_user
     @comment.save
