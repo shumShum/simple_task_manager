6f2cf98e86c8015e907f52c3a10a53dfa52e74ed
Shumilkin Gena <shumilkin.gena@yandex.ru>
2013-08-28 13:32:35 +0400

    add cocooon comment work

--- app/types/story_edit_type.rb
+++ app/types/story_edit_type.rb
@@ -5,4 +5,13 @@ class StoryEditType < Story
   attr_accessible :title, :body, :assigner_id, :assignee_id, :pic, :comments_attributes, :state_event
 
   has_many :comments, class_name: CommentNewType
+
+  def comments_attributes=(attrs)
+    attrs.each do |key, value|
+      if !value.has_key?(:id)
+        value[:user_id] = current_user.id
+      end
+    end
+    super
+  end
 end
